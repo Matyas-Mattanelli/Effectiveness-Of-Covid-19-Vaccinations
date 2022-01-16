c90508338d083328d16ace15832f2024848a5309
Matyas-Mattanelli <91661261+Matyas-Mattanelli@users.noreply.github.com>
2022-01-16 14:32:44 +0100

    Advancing the analysis

--- Script.R
+++ Script.R
@@ -39,10 +39,38 @@ dataset_monthly$date<-as.Date(paste("01",dataset_monthly$month,dataset_monthly$y
 dataset_semifinal<-dataset_monthly[,c(1,10,4:9)]
 dataset_semifinal<-dataset_semifinal[order(dataset_semifinal$location,dataset_semifinal$date),]
 
+#Inspecting the data set
+summary(dataset_semifinal)
+dataset_semifinal$new_cases[dataset_semifinal$new_cases<0]<-NA #Negative cases do not make sense
+
+#Histograms
+hist(dataset_semifinal$new_cases) #Extremely skewed
+hist(log(dataset_semifinal$new_cases+1)) #A bit better, need to add a constant due to zeros
+
+hist(dataset_semifinal$new_tests) #Heavily skewed
+hist(log(dataset_semifinal$new_tests))
+
+hist(dataset_semifinal$new_vaccinations) #Skeeeewed
+hist(log(dataset_semifinal$new_vaccinations+1)) #A lot better
+
 #Preliminary analysis
 library(plm)
-model <- plm(new_cases~new_tests+new_vaccinations,data=dataset_semifinal,model="within",effect = "twoways",
-             index=c("location","date"))
-summary(model) #wohoo, vaccinations coefficient negative!
+model <- plm(log(new_cases+1)~lag(log(new_cases+1),1)+lag(log(new_tests),0:1)+lag(log(new_vaccinations+1),0:4),data=dataset_semifinal,
+             model="within",effect = "twoways",index=c("location","date"))
+summary(model) 
 
+#Heteroskedasticity
+library(lmtest)
+bptest(model)
 
+#Serial correlation
+pbgtest(model)
+
+#Clustered robust standard errors
+library(sandwich)
+coeftest(model,vcov=vcovHC(model,type = "HC0",cluster = "group"))
+
+#Difference GMM (lag is very significant => dynamic panel data analysis)
+diff_model<-pgmm(log(new_cases+1)~lag(log(new_cases+1),1)+log(new_tests)+log(new_vaccinations+1)|lag(log(new_cases+1),2:10),
+                 data=dataset_semifinal,effect = "twoways",model=c("twosteps"),transformation = "d",index=c("location","date"))
+summary(diff_model) #System is computationally singlar when I add vaccinations
