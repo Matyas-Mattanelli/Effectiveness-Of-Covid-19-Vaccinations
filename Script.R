1ca135a84f63baffaf1996db6368c80e3f0302c1
Matyas-Mattanelli <91661261+Matyas-Mattanelli@users.noreply.github.com>
2022-01-21 14:05:51 +0100

    Enhancing the data description part

--- Script.R
+++ Script.R
@@ -7,9 +7,11 @@ dataset <- as.data.frame(dataset)
 ### Data inspection ###
 #######################
 
-#Descriptive statistics
-summary(dataset)
+#Inspecting the data set
+summary(dataset) #Negative cases + Vaccination many missing values
 dataset$new_cases[dataset$new_cases<0]<-NA #Negative cases do not make sense
+min(na.omit(dataset[,c(1:5,9)])$date) #Earliest vaccination data in December 2020
+dataset<-dataset[dataset$date>=as.Date("2020-10-01"),] #We retain some past values given the inclusion of lags in the analysis
 
 #Scaling by population (for increased cross-country comparability)
 dataset$new_cases_per_thousand<-dataset$new_cases/(dataset$population/1000)
@@ -17,24 +19,49 @@ dataset$new_vaccinations_per_thousand<-dataset$new_vaccinations/(dataset$populat
 dataset$new_tests_per_thousand<-dataset$new_tests/(dataset$population/1000)
 
 #Histograms
-hist(dataset$new_cases_per_thousand) #Extremely skewed
-hist(log(dataset$new_cases_per_thousand+min(dataset$new_cases_per_thousand[dataset$new_cases_per_thousand>0],na.rm = T))) #A bit better, need to add a constant due to zeros
+#Defining a trimming function to make the histograms nicer
+trim <- function(x){
+  x[(x > quantile(x, 0.25,na.rm=T)-1.5*IQR(x,na.rm=T)) & (x < quantile(x, 0.75,na.rm=T)+1.5*IQR(x,na.rm=T))]
+}
 
-hist(dataset$new_tests_per_thousand) #Heavily skewed
-hist(log(dataset$new_tests_per_thousand))
+par(mfrow=c(4,2))
+hist(trim(dataset$new_cases_per_thousand),border = F,col="grey",main = "New Covid-19 cases per thousand (level)",xlab = "",ylab="") #Extremely skewed, many outliers
+hist(trim(log(dataset$new_cases_per_thousand+min(dataset$new_cases_per_thousand[dataset$new_cases_per_thousand>0],na.rm = T))),border=F,col="grey",xlab="",ylab="",main="New Covid-19 cases per thousand (log)") #A bit better, need to add a constant due to zeros
 
-hist(dataset$new_vaccinations) #Skeeeewed
-hist(log(dataset$new_vaccinations+min(dataset$new_vaccinations_per_thousand[dataset$new_vaccinations_per_thousand>0],na.rm = T))) #A lot better
+hist(trim(dataset$new_tests_per_thousand),border = F,col="grey",main = "New Covid-19 tests per thousand (level)",xlab = "",ylab="") #Heavily skewed
+hist(trim(log(dataset$new_tests_per_thousand)),border=F,col="grey",xlab="",ylab="",main="New Covid-19 tests per thousand (log)")
 
-hist(dataset$avg_temp) #A bit left-skewed (but log won't help)
+hist(trim(dataset$new_vaccinations_per_thousand),border = F,col="grey",main = "New Covid-19 vaccinations per thousand (level)",xlab = "",ylab="") #Skeeeewed
+hist(trim(log(dataset$new_vaccinations_per_thousand+min(dataset$new_vaccinations_per_thousand[dataset$new_vaccinations_per_thousand>0],na.rm = T))),border=F,col="grey",xlab="",ylab="",main="New Covid-19 vaccinations per thousand (log)") #A lot better
 
-#Transforming the variables accordingly
+hist(trim(dataset$avg_temp),border=F,col="grey",main = "Average temperature",xlab = "",ylab="") #A bit left-skewed (but log won't help)
+
+#Transforming the variables
 dataset$log_new_cases_per_thousand <- log(dataset$new_cases_per_thousand+min(dataset$new_cases_per_thousand[dataset$new_cases_per_thousand>0],na.rm = T))
 dataset$log_new_tests_per_thousand <- log(dataset$new_tests_per_thousand)
 dataset$log_new_vaccinations_per_thousand <- log(dataset$new_vaccinations+min(dataset$new_vaccinations_per_thousand[dataset$new_vaccinations_per_thousand>0],na.rm = T))
 
+#Retaining only relevant columns for brevity
+dataset_final<-dataset[,c(1,2,13:15,9)]
+
+#Descriptive statistics
+library(stargazer)
+stargazer(na.omit(dataset[,c(10:12,9)]),title = "Descriptive statistics",label="tab:des",header = F,
+          summary.stat = c("mean","sd","min","median","max"),
+          notes = "Note: All variables apart from Average Temperature are in per thousand people terms")
+length(unique(na.omit(dataset)$location)) #103 countries
+table(table(na.omit(dataset)$location)) #1 to 13 periods
+
 #Correlation matrix
-cor_mat<-cor(dataset[,c(13:15,9)],use="pairwise.complete.obs")
+library(rstatix)
+cor_mat<-cor_mat(dataset[,c(13:15,9)])
+cor_mat<-cor_mark_significant(cor_mat,cutpoints = c(0,0.05,1),symbols = c("*",""))
+for (i in 1:4){
+  cor_mat[i,i+1]<-1
+}
+stargazer(cor_mat,summary = F,header = F,title = "Correlation matrix",
+          notes = c("Note: All variables apart from Average temperature are in logs per thousand people","* significant at 95%"),
+          label="tab:cor_mat",rownames = F)
 
 ################
 ### Analysis ###
@@ -117,3 +144,14 @@ mtest(diff_model,3) #No third order serial correlation => instruments valid
 model_fe_sens <- plm(log_new_cases_per_thousand~lag(log_new_cases_per_thousand,1:2)+log_new_tests_per_thousand+lag(log_new_vaccinations_per_thousand,0:3)+avg_temp,
                 data=dataset,model="within",effect = "twoways",index=c("location","date"))
 summary(model_fe_sens,vcov=vcovHC(model_fe,method="arellano",cluster="group")) 
+
+indicator<-table(dataset_final_no_nas$location)>=12
+complet_countries<-names(indicator)[indicator]
+complet_countries
+
+dataset_test<-dataset[dataset$location %in% complet_countries,]
+
+diff_test_model <- pgmm(log_new_cases_per_thousand~lag(log_new_cases_per_thousand,1:2)+lag(log_new_tests_per_thousand,0:1)+lag(log_new_vaccinations_per_thousand,0:4)+avg_temp|lag(log_new_cases_per_thousand,3:7),
+                        data=na.omit(dataset),effect = "twoways",model=c("twosteps"),transformation = "ld",index=c("location","date"))
+summary(diff_test_model)
+mtest(diff_test_model,3)
